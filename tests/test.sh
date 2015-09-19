6c118f757643edb038fdc5e9c015973f8e9d0a3d
alonisser <alonisser@gmail.com>
2015-09-19 20:38:41 +0300

    fail fast

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-ansible-playbook --syntax-check --list-tasks -i tests/hosts tests/playbook.yml
-ansible-playbook -i tests/hosts tests/playbook.yml | grep -q 'changed=1.*failed=0' &&
- (echo 'first change: pass' && exit 0) || (echo 'first change: fail' && exit 1)
+set -e
+
+ansible-playbook --syntax-check --list-tasks -i tests/hosts tests/playbook.yml &&
+ (echo 'syntax check: pass' && exit 0) || (echo 'syntax check: fail' && exit 1)
+
+ansible-playbook -i tests/hosts tests/playbook.yml
 
 stat /etc/fail2ban/filter.d/nginx-badbots.conf
 stat /etc/fail2ban/jail.d/nginx-jails.conf
+
 sudo fail2ban-client status | grep "Number of jail" | cut -d ":" -f 2 | grep 5 &&
  (echo 'jail number test 5: pass' && exit 0) || (echo 'jail number test 5: fail' && exit 1)
 
