2e9641f158fb8b317cc321ac6cc9c3f8ac41ef08
alonisser <alonisser@gmail.com>
2015-09-19 21:08:39 +0300

    detailed failure

--- tests/test.sh
+++ tests/test.sh
@@ -9,7 +9,7 @@ ansible-playbook -i tests/hosts tests/playbook.yml
 stat /etc/fail2ban/filter.d/nginx-badbots.conf
 stat /etc/fail2ban/jail.d/nginx-jails.conf
 
-sudo fail2ban-client status | grep "Number of jail" #| cut -d ":" -f 2 | grep 5 &&
+sudo fail2ban-client status #| grep "Number of jail" | cut -d ":" -f 2 | grep 5 &&
  #(echo 'jail number test 5: pass' && exit 0) || (echo 'jail number test 5: fail' && exit 1)
 
 ansible-playbook -i tests/hosts tests/playbook.yml   | grep -q 'changed=0.*failed=0' &&
